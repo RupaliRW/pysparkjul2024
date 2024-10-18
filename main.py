24df55b64410358219acc978bb8328a42af8c0dc
RupaliRW <92073945+RupaliRW@users.noreply.github.com>
2024-10-18 19:12:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,6 @@ if __name__ == '__main__':
     for (w, c) in res:
         print(w + " -- " + str(c))
     print("done*******************************")
-    print("18-10-2024")
+    print("18-10-2024 7::11 pm IST")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
